9f112a92083f41b844a08dc94de583bdca6ac4bb
Robert West <robert.w.coding@gmail.com>
2021-04-19 15:34:44 +0100

    Initial commit

---
+++ GetWeatherIcons.py
@@ -0,0 +1,21 @@
+import os
+import urllib.request
+
+def getWeatherIcons():
+    day = ["01d.png", "02d.png", "03d.png", "04d.png", "09d.png", "10d.png", "11d.png", "13d.png", "50d.png"]
+    night = ["01n.png", "02n.png", "03n.png", "04n.png", "09n.png", "10n.png", "11n.png", "13n.png", "50n.png"]
+
+    baseURL = "https://openweathermap.org/img/w/"
+    imgFolder = "./weatherIcons/"
+    if not os.path.exists(imgFolder):
+        os.makedirs(imgFolder)
+
+    for name in day:
+        fileName = imgFolder + name
+        if not os.path.exists(fileName):
+            urllib.request.urlretrieve(baseURL + name, fileName)
+
+    for name in night:
+        fileName = imgFolder + name
+        if not os.path.exists(fileName):
+            urllib.request.urlretrieve(baseURL + name, fileName)
---
+++ WeatherApp.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+from tkinter import font
+import requests
+from PIL import Image, ImageTk
+from GetWeatherIcons import getWeatherIcons
+
+# e4463432d83bca00bbe2ebfcb4eb6f56
+# api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={your api key}
+
+root = tk.Tk()
+
+HEIGHT = 520
+WIDTH = 680
+
+def formatResponse(weather):
+    try:
+        name = weather["name"]
+        desc = weather["weather"][0]["description"]
+        temp = weather["main"]["temp"]
+        output = "City: %s \nConditions: %s \nTemperature (C): %s" % (name, desc, temp)
+    except:
+        output = "Cannot find weather data for that place"
+
+    return output
+
+def getWeather(city):
+    weatherKey = "e4463432d83bca00bbe2ebfcb4eb6f56"
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {"APPID": weatherKey, "q": city, "units": "metric"}
+    response = requests.get(url, params=params)
+    weather = response.json()
+
+    results["text"] = formatResponse(weather)
+
+    getWeatherIcons()
+    imageName = weather["weather"][0]["icon"]
+    openImage(imageName)
+
+def openImage(fileName):
+    size = int(lowerFrame.winfo_height()*0.25)
+    icon = ImageTk.PhotoImage(Image.open("./weatherIcons/" + fileName + ".png").resize((size, size)))
+    weatherIcon.delete("all")
+    weatherIcon.create_image(0, 0, anchor='nw', image=icon)
+    weatherIcon.image = icon
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+#backgroundImage = tk.PhotoImage(file="Combinatorics with Edgeworth.PNG")
+#backgroundLabel = tk.Label(root, image=backgroundImage)
+#backgroundLabel.place(x=0, y=0, relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg="#85c2ff", bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor="n")
+
+entry = tk.Entry(frame, font=("Courier", 18))
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="Get Weather", bg="gray", fg="red", font=("Courier", 12), command=lambda: getWeather(entry.get()))
+button.place(relx=0.7, relwidth=0.3, relheight=1)
+
+lowerFrame = tk.Frame(root, bg="#85c2ff", bd=10)
+lowerFrame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor="n")
+
+results = tk.Label(lowerFrame, font=("Courier", 14), anchor="nw", justify="left", bd=4)
+results.place(relwidth=1, relheight=1)
+
+weatherIcon = tk.Canvas(results, bd=0, highlightthickness=0)
+weatherIcon.place(relx=0.75, rely=0, relwidth=1, relheight=0.5)
+
+root.mainloop()
